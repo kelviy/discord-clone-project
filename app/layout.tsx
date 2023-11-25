39d8cd50f44f566dafdd683e99e1a2419b25a436
kelviy <52874397+kelviy@users.noreply.github.com>
2023-11-25 12:06:28 +0200

    Completed Initial Setup of prima database. Video Time Stamp [1:25:22]

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
+            forcedTheme="dark"
             enableSystem={false}
             disableTransitionOnChange
             storageKey="discord-theme"
